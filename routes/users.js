15028cd61ee3652f288432dc3b9550eee40ec876
Siyami Avci <siyamiavci@gmail.com>
2017-01-09 22:52:15 -0800

    add solution for authentication

--- routes/users.js
+++ routes/users.js
@@ -1,32 +1,100 @@
+// 'use strict';
+//
+// const bcrypt = require('bcrypt-as-promised');
+// const express = require('express');
+// const knex = require('../knex');
+// const {
+//   camelizeKeys
+// } = require('humps');
+//
+// const router = express.Router();
+//
+// router.post('/users', (req, res, next) => {
+//   bcrypt.hash(req.body.password, 12)
+//     .then((hashed_password) => {
+//       return knex('users')
+//         .insert({
+//           first_name: req.body.firstName,
+//           last_name: req.body.lastName,
+//           email: req.body.email,
+//           hashed_password: hashed_password
+//         }, '*');
+//     })
+//     .then((users) => {
+//       const user = users[0];
+//
+//       delete user.hashed_password;
+//
+//       res.set('Content-Type', 'application/json');
+//       res.send(camelizeKeys(user));
+//     })
+//     .catch((err) => {
+//       next(err);
+//     });
+// });
+//
+// module.exports = router;
+
+
+//RYAN'S SOLUTION
+
+
 'use strict';
 
 const bcrypt = require('bcrypt-as-promised');
+const boom = require('boom');
 const express = require('express');
+const jwt = require('jsonwebtoken');
 const knex = require('../knex');
 const {
-  camelizeKeys
+  camelizeKeys, decamelizeKeys
 } = require('humps');
 
+// eslint-disable-next-line new-cap
 const router = express.Router();
 
 router.post('/users', (req, res, next) => {
-  bcrypt.hash(req.body.password, 12)
-    .then((hashed_password) => {
-      return knex('users')
-        .insert({
-          first_name: req.body.firstName,
-          last_name: req.body.lastName,
-          email: req.body.email,
-          hashed_password: hashed_password
-        }, '*');
+  const {
+    email, password
+  } = req.body;
+
+  if (!email || !email.trim()) {
+    return next(boom.create(400, 'Email must not be blank'));
+  }
+
+  if (!password || password.length < 8) {
+    return next(boom.create(
+      400,
+      'Password must be at least 8 characters long'
+    ));
+  }
+
+  knex('users')
+    .where('email', email)
+    .first()
+    .then((user) => {
+      if (user) {
+        throw boom.create(400, 'Email already exists');
+      }
+
+      return bcrypt.hash(password, 12);
     })
-    .then((users) => {
-      const user = users[0];
+    .then((hashedPassword) => {
+      const {
+        firstName, lastName
+      } = req.body;
+      const insertUser = {
+        firstName, lastName, email, hashedPassword
+      };
 
-      delete user.hashed_password;
+      return knex('users').insert(decamelizeKeys(insertUser), '*');
+    })
+    .then((rows) => {
+      const user = camelizeKeys(rows[0]);
 
-      res.set('Content-Type', 'application/json');
-      res.send(camelizeKeys(user));
+      delete user.hashedPassword;
+
+      res.send(user);
     })
     .catch((err) => {
       next(err);
